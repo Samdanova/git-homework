04285025d1277a896e3ff0f895046fd8d22e1562
Samdanova <samdanova123@gmail.com>
2024-03-10 21:05:40 +0200

    scope

---
+++ week-3/scope/index.js
@@ -0,0 +1,65 @@
+
+// const run = true
+
+// if (run) {
+//     let distance = 8
+//     for (var i = 0; i < distance; i++) {
+//         console.log("running")
+//     }
+//     console.log("Finished running " + distance + " miles")
+// }
+
+// console.log("Damn, you see this gal? She ran " + distance + " miles") 
+
+
+// if (13 == "space") {
+//     let cowSound = "moo"
+// }
+// else {
+//     console.log("Cow says " + cowSound)
+// }
+
+
+// const serveOrders = function (orders) {
+
+//     for (let order of orders) {
+//         let specialOrder = "special " + order
+//         console.log("Served a " + specialOrder)
+//     }
+
+//     console.log("Finished serving all the orders: " + orders)
+// }
+// const allOrders = ["fish", "lettuce plate", "curious cheese"]
+// serveOrders(allOrders)
+
+// const pot = "red pot with earth in it"
+
+// const getSeed = function () {
+//     const seed = "brown seed"
+// }
+
+// const plant = function () {
+//     getSeed()
+//     console.log("Planting the " + seed + " inside a " + pot)
+// }
+
+// plant()
+
+
+const isEnough = false
+
+const makeEnough = function () {
+    for (let i = 0; i < 10; i++) {
+        if (i > 5) {
+            isEnough = true
+        }
+    }
+}
+
+makeEnough()
+if (isEnough) {
+    console.log("Finally, sheesh")
+}
+else {
+    console.log("Here we go again...")
+}
