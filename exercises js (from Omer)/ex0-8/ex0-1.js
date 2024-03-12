c76b9f6cc58447b2963ae10ecf6cdefa0a3eb111
Samdanova <samdanova123@gmail.com>
2024-03-12 20:34:21 +0200

    correct format

--- exercises js (from Omer)/ex0-8/ex0-1.js
+++ exercises js (from Omer)/ex0-8/ex0-1.js
@@ -1,18 +1,16 @@
-
-
-let add="hello";
+let add = "hello";
 console.log(typeof(a))
 
-let number=5;
+let number = 5;
 console.log(typeof(number))
 
-let object={};
+let object = {};
 console.log(typeof(object))
 
-let boolean=true;
+let boolean = true;
 console.log(typeof(boolean))
 
-let c=null;
+let c = null;
 console.log(typeof(c))
 
 let u;
@@ -21,35 +19,28 @@ console.log(typeof(u))
 let mySymbol = Symbol('uniqueSymbol');
 console.log(typeof(mySymbol))
 
-
 //create three different ways to declare variables
-var x=5;
-let y=5;
-const t=5;
+var x = 5;
+let y = 5;
+const t = 5;
 
 //declare a variable and reassign a value to it
-var x=7; 
-// - possible only with var
-
-//create a variable and after that assign a string
+var x = 7;
+// - possible only with var create a variable and after that assign a string
 // with its value being: He's got it!
 let string;
-string="He's got it"
+string = "He's got it"
 
-
-// 8. create a variable and assign a value on how
-// much a restaurant bill is.
-let bill=500;
-// 9. create a variable and assign a value on how
-// much tax they should pay.
-let tax=0.13
+// 8. create a variable and assign a value on how much a restaurant bill is.
+let bill = 500;
+// 9. create a variable and assign a value on how much tax they should pay.
+let tax = 0.13
 // 10. create a variable that will calculate the bill
-let totalSum=bill+bill*tax;
-// - tax and its output would be: Your total bill is
-// <total bill> $.
-let totalBill="Your total bill is "+totalSum;
+let totalSum = bill + bill * tax;
+// - tax and its output would be: Your total bill is <total bill> $.
+let totalBill = "Your total bill is " + totalSum;
 
-// // Round the number 50.6 to its nearest integer
+//  Round the number 50.6 to its nearest integer
 let roundedNumber = Math.round(50.6);
-// //Create a variable that is undefined
+// Create a variable that is undefined
 let variable;
--- exercises js (from Omer)/ex0-8/ex8.js
+++ exercises js (from Omer)/ex0-8/ex8.js
@@ -1,20 +1,19 @@
 //ex #8
 
-function percentageOfWorld1(population){
-    const overall=7900;
-    let result=(population/overall*100).toFixed(2);
+function percentageOfWorld1(population) {
+    const overall = 7900;
+    let result = (population / overall * 100).toFixed(2);
     console.log(result + "%")
 }
 percentageOfWorld1(1141);
 percentageOfWorld1(2000);
 percentageOfWorld1(500);
 
-const percentageOfWorld2 = (population)=>{
-    const overall=7900;
-    let result=(population/overall*100).toFixed(2);
+const percentageOfWorld2 = (population) => {
+    const overall = 7900;
+    let result = (population / overall * 100).toFixed(2);
     console.log(result + "%")
 }
 percentageOfWorld2(1141);
 percentageOfWorld2(2000);
 percentageOfWorld2(500);
-
